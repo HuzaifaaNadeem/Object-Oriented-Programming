958b5fd4bafa0ffc2f5e28114a063603a95c029c
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2025-03-03 09:19:04 +0500

    Add files via upload

--- LAB_5/Question1.cpp
+++ LAB_5/Question1.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+using namespace std;
 
+class Square {
+private:
+    float sideLength;
+    float area;
+    static float allareas;
+
+public:
+    Square() : sideLength(0.0), area(0.0) {}
+
+    Square(float side) : sideLength(side), area(0.0) {}
+
+    void calculate_area() {
+        area = sideLength * sideLength;
+        allareas += area;
+    }
+
+    float get_sideLength() const {
+        return sideLength;
+    }
+
+    float get_area() const {
+        return area;
+    }
+
+    static float get_allareas() {
+        return allareas;
+    }
+
+    void set_sideLength(float side) {
+        sideLength = side;
+    }
+
+    void set_area(float a) {
+        area = a;
+    }
+};
+
+float Square::allareas = 0.0;
+
+int main() {
+    Square square1(4.0);
+    Square square2(5.0);
+    Square square3(6.0);
+    
+    square1.calculate_area();
+    cout << "Square 1: Area: " << square1.get_area() << ", Total of all Areas: " << Square::get_allareas() << endl;
+    
+    square2.calculate_area();
+    cout << "Square 2: Area: " << square2.get_area() << ", Total of all Areas: " << Square::get_allareas() << endl;
+    
+    square3.calculate_area();
+    cout << "Square 3: Area: " << square3.get_area() << ", Total of all Areas: " << Square::get_allareas() << endl;
+    
+    return 0;
+}
