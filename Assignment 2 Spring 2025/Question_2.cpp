c98d41f5a61f53d92015ef421ec06d25b046f9e2
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2025-03-30 12:46:31 +0500

    Update Question_2.cpp

--- Assignment 2 Spring 2025/Question_2.cpp
+++ Assignment 2 Spring 2025/Question_2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 class Vehicle {
@@ -7,51 +7,64 @@ protected:
     string name;
     int speed;
     int capacity;
+    static int total_deliveries;
 
 public:
-    Vehicle(string n, int s, int c) : name(n), speed(s), capacity(c) {}
-    virtual void deliver(int weight, string urgency, string destination) {
-        cout << name << " is delivering a " << weight << "kg package with " << urgency << " urgency to " << destination << "...\n";
+    Vehicle(string n, int s, int c) : name(n), speed(s), capacity(c) { total_deliveries++; }
+    virtual void deliver(int package_id, string urgency, string destination) {
+        cout << name << " is delivering package " << package_id << " with " << urgency << " urgency to " << destination << "...\n";
     }
-    virtual void efficiency_metrics() {
-        cout << "Calculating efficiency metrics for " << name << "...\n";
+    virtual void calculate_route() {
+        cout << name << " is calculating the optimal route...\n";
     }
-    bool operator>(const Vehicle &v) {
-        return speed > v.speed;
+    virtual void estimate_delivery_time() {
+        cout << name << " is estimating delivery time...\n";
+    }
+    bool operator==(const Vehicle &v) {
+        return (speed == v.speed && capacity == v.capacity);
     }
     friend void resolve_conflict(Vehicle &v1, Vehicle &v2);
 };
 
+int Vehicle::total_deliveries = 0;
+
 class RamzanTimeShip : public Vehicle {
 public:
     RamzanTimeShip() : Vehicle("Ramzan TimeShip", 300, 500) {}
-    void deliver(int weight, string urgency, string destination) override {
-        cout << name << " is traveling through time to swiftly deliver a " << weight << "kg package with " << urgency << " urgency to " << destination << "!\n";
+    void deliver(int package_id, string urgency, string destination) override {
+        cout << name << " is validating historical accuracy before delivering package " << package_id << " to " << destination << " with " << urgency << " urgency!\n";
     }
 };
 
 class RamzanDrone : public Vehicle {
 public:
     RamzanDrone() : Vehicle("Ramzan Drone", 150, 50) {}
-    void deliver(int weight, string urgency, string destination) override {
-        cout << name << " is soaring through the sky to quickly deliver a " << weight << "kg package with " << urgency << " urgency to " << destination << "!\n";
+    void deliver(int package_id, string urgency, string destination) override {
+        cout << name << " is activating high-speed mode to ensure package " << package_id << " reaches " << destination << " in time for " << urgency << "!\n";
     }
 };
 
 class RamzanHyperPod : public Vehicle {
 public:
     RamzanHyperPod() : Vehicle("Ramzan HyperPod", 200, 1000) {}
-    void deliver(int weight, string urgency, string destination) override {
-        cout << name << " is using hyperloop speed to get a " << weight << "kg package to " << destination << " with " << urgency << " urgency!\n";
+    void deliver(int package_id, string urgency, string destination) override {
+        cout << name << " is navigating underground tunnels to deliver package " << package_id << " to " << destination << " with " << urgency << " urgency efficiently!\n";
     }
 };
 
 void resolve_conflict(Vehicle &v1, Vehicle &v2) {
-    cout << "Oh no! A scheduling conflict between " << v1.name << " and " << v2.name << "! Let's see who gets priority...\n";
-    if (v1 > v2)
-        cout << v1.name << " is assigned higher priority for this delivery.\n";
+    cout << "Conflict detected between " << v1.name << " and " << v2.name << "! Determining the best delivery option...\n";
+    if (v1.speed > v2.speed)
+        cout << v1.name << " is assigned higher priority due to faster speed.\n";
     else
-        cout << v2.name << " is assigned higher priority for this delivery.\n";
+        cout << v2.name << " is assigned higher priority due to better efficiency.\n";
+}
+
+void command(string action, int package_id) {
+    cout << "Operator command received: " << action << " package " << package_id << "\n";
+}
+void command(string action, int package_id, string urgency) {
+    cout << "Operator command received: " << action << " package " << package_id << " with " << urgency << " urgency\n";
 }
 
 int main() {
@@ -59,11 +72,14 @@ int main() {
     RamzanDrone drone;
     RamzanHyperPod hyperPod;
     
-    cout << "Ramzan delivery system initializing...\n\n";
+    cout << "Ramzan Box Delivery System initializing...\n\n";
     
-    timeShip.deliver(10, "Sehri", "Karachi");
-    drone.deliver(5, "Iftar", "Lahore");
-    hyperPod.deliver(50, "Normal", "Islamabad");
+    command("Deliver", 101);
+    command("Deliver", 102, "Sehri");
+    
+    timeShip.deliver(101, "Normal", "Karachi");
+    drone.deliver(102, "Iftar", "Lahore");
+    hyperPod.deliver(103, "Sehri", "Islamabad");
     
     cout << "\nResolving delivery conflicts...\n";
     resolve_conflict(drone, hyperPod);
