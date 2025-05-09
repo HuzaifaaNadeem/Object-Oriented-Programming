b9c339da8f8eb3620b197dda8a108acaa4f14944
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2025-05-09 22:45:19 +0500

    Update Question_1.cpp

--- LAB_10/Question_1.cpp
+++ LAB_10/Question_1.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    float gpa;
+};
+
+void write(Student s[], int n) {
+    ofstream out("students.txt", ios::out);
+    for (int i = 0; i < n; ++i)
+        out << s[i].id << " " << s[i].name << " " << s[i].gpa << "\n";
+    out.close();
+}
+
+void append(Student s) {
+    ofstream out("students.txt", ios::app);
+    out << s.id << " " << s.name << " " << s.gpa << "\n";
+    out.close();
+}
+
+void read() {
+    ifstream in("students.txt");
+    Student s;
+    while (in >> s.id >> s.name >> s.gpa)
+        cout << s.id << " " << s.name << " " << s.gpa << endl;
+    in.close();
+}
+
+int main() {
+    Student s[5];
+    for (int i = 0; i < 5; ++i)
+        cin >> s[i].id >> s[i].name >> s[i].gpa;
+
+    write(s, 5);
+
+    Student extra;
+    cin >> extra.id >> extra.name >> extra.gpa;
+    append(extra);
+
+    read();
+
+    return 0;
+}
 
