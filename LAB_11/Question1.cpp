397bd0ef1dea565ff9571144df3f1e7e9e2f011a
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2025-05-09 08:04:58 +0500

    Update Question1.cpp

--- LAB_11/Question1.cpp
+++ LAB_11/Question1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+bool validate_age(int age) {
+    if (age < 0 || age > 120) {
+        cout << "Error: InvalidValueException - Age cannot be negative or exceed 120." << endl;
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    if (validate_age(age)) {
+        cout << "Valid age entered: " << age << endl;
+    }
+
+    return 0;
+}
 
